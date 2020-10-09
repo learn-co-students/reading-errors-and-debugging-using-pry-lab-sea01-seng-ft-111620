d049e2c3b6437c7b5260ee3fedcb6aece88d62de
Alexander Bowers <7319abowers@gmail.com>
2020-10-09 18:59:23 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,21 @@
 # don't forget to add: require 'pry'
 
+#def snake_it_up(string)
+#  if string[0] == "s"
+  #  "s" + string
+#  else
+#    string
+#  end
+#end
+
+
+
+
 def snake_it_up(string)
-  if string[0] == "s"
-    10 * "s" + string
-  else
-    string
+  counter = 0
+  until counter == 10
+    string.prepend "s"
+    counter += 1
   end
+  string
 end
